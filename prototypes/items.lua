77e1790e5266a904e9a992ed6ea1ff30d364234d
ichvii <57349267+ichvii@users.noreply.github.com>
2019-11-06 19:58:31 +0100

    Update items.lua

--- prototypes/items.lua
+++ prototypes/items.lua
@@ -60,4 +60,42 @@ data:extend{
     order = "b[combinators]-d[large-combinator]",
     stack_size = 50,
   },
+  {
+    type = "item",
+    name = "alphabet-combinator",
+    icons = {
+      { icon = "__base__/graphics/icons/constant-combinator.png", icon_size = 32, },
+      { icon = "__base__/graphics/icons/signal/signal_A.png", icon_size = 32, scale = 0.6, },
+    },
+    subgroup = "circuit-network",
+    place_result="alphabet-combinator",
+    order = "b[combinators]-d[alphabet-combinator]",
+    stack_size = 50,
+  },
+  {
+    type = "item",
+    name = "stacksize+-combinator",
+    icons = {
+      { icon = "__base__/graphics/icons/constant-combinator.png", icon_size = 32, },
+      { icon = "__base__/graphics/icons/signal/signal_S.png", icon_size = 32, scale = 0.5, shift = {-8,0} },
+      { icon = "__base__/graphics/icons/signal/signal_P.png", icon_size = 32, scale = 0.5, shift = {8,0} },
+    },
+    subgroup = "circuit-network",
+    place_result="large-combinator",
+    order = "b[combinators]-d[stacksize+-combinator]",
+    stack_size = 50,
+  },
+    {
+    type = "item",
+    name = "stacksize--combinator",
+    icons = {
+      { icon = "__base__/graphics/icons/constant-combinator.png", icon_size = 32, },
+      { icon = "__base__/graphics/icons/signal/signal_S.png", icon_size = 32, scale = 0.5, shift = {-8,0} },
+      { icon = "__base__/graphics/icons/signal/signal_M.png", icon_size = 32, scale = 0.5, shift = {8,0} },
+    },
+    subgroup = "circuit-network",
+    place_result="large-combinator",
+    order = "b[combinators]-d[stacksize--combinator]",
+    stack_size = 50,
+  },
   }
