29954ccaf927f4ee5e88e077e33f66c332116e74
ichvii <57349267+ichvii@users.noreply.github.com>
2019-11-06 20:08:15 +0100

    Update utility-combinators_0.17.1.lua

--- migrations/utility-combinators_0.17.1.lua
+++ migrations/utility-combinators_0.17.1.lua
@@ -3,4 +3,7 @@ for _,force in pairs(game.forces) do
   force.recipes["location-combinator"].enabled = force.technologies["circuit-network-2"].researched
   force.recipes["player-combinator"].enabled = force.technologies["circuit-network-2"].researched
   force.recipes["research-combinator"].enabled = force.technologies["circuit-network-2"].researched
+  force.recipes["alphabet-combinator"].enabled = force.technologies["circuit-network-2"].researched
+  force.recipes["stacksize+-combinator"].enabled = force.technologies["circuit-network-2"].researched
+  force.recipes["stacksize--combinator"].enabled = force.technologies["circuit-network-2"].researched
 end
