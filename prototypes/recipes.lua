c60539ad8cabed563e898a5bf7ecf1e54f3b8c74
ichvii <57349267+ichvii@users.noreply.github.com>
2020-04-22 11:41:03 +0200

    removed justarandomgeeks combinators

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -1,50 +1,6 @@
 data:extend{
   {
     type = "recipe",
-    name = "location-combinator",
-    enabled = "false",
-    ingredients =
-    {
-      {"constant-combinator", 1},
-      {"electronic-circuit", 1},
-    },
-    result="location-combinator",
-  },
-  {
-    type = "recipe",
-    name = "bonus-combinator",
-    enabled = "false",
-    ingredients =
-    {
-      {"constant-combinator", 1},
-      {"electronic-circuit", 1},
-    },
-    result="bonus-combinator",
-  },
-  {
-    type = "recipe",
-    name = "player-combinator",
-    enabled = "false",
-    ingredients =
-    {
-      {"constant-combinator", 1},
-      {"electronic-circuit", 1},
-    },
-    result="player-combinator",
-  },
-  {
-    type = "recipe",
-    name = "research-combinator",
-    enabled = "false",
-    ingredients =
-    {
-      {"constant-combinator", 1},
-      {"electronic-circuit", 1},
-    },
-    result="research-combinator",
-  },
-  {
-    type = "recipe",
     name = "large-combinator",
     enabled = "false",
     ingredients =
