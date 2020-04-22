e2c269bd9a6cd50966416ee7c6eaf1241c3fa77b
ichvii <57349267+ichvii@users.noreply.github.com>
2020-04-22 11:39:14 +0200

    Update entities.lua

--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -1,32 +1,7 @@
-local p = table.deepcopy(data.raw["constant-combinator"]["constant-combinator"])
-p.name = "location-combinator"
-p.minable.result = "location-combinator"
-
-data:extend{p}
-
-local entity = table.deepcopy(data.raw["constant-combinator"]["constant-combinator"])
-entity.name = "bonus-combinator"
-entity.minable.result = "bonus-combinator"
-entity.item_slot_count = 30
-data:extend{entity}
-
-local entity = table.deepcopy(data.raw["constant-combinator"]["constant-combinator"])
-entity.name = "player-combinator"
-entity.minable.result = "player-combinator"
-entity.item_slot_count = 30
-data:extend{entity}
-
-local entity = table.deepcopy(data.raw["constant-combinator"]["constant-combinator"])
-entity.name = "research-combinator"
-entity.minable.result = "research-combinator"
-entity.item_slot_count = 30
-
-data:extend{entity}
-
 local entity = table.deepcopy(data.raw["constant-combinator"]["constant-combinator"])
 entity.name = "large-combinator"
 entity.minable.result = "large-combinator"
-entity.item_slot_count = 1000
+entity.item_slot_count = 10000
 data:extend{entity}
 
 local entity = table.deepcopy(data.raw["constant-combinator"]["constant-combinator"])
